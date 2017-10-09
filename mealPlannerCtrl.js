3ae09eec3edb3bd550949c6bb600067d35394b81
Sam Rudd <samdrudd@gmail.com>
2017-10-09 03:59:58 -0700

    try again?

--- mealPlannerCtrl.js
+++ mealPlannerCtrl.js
@@ -1,9 +1,14 @@
 app.controller("mealPlannerCtrl", function($scope) {
 	$scope.recipelist = {};
+	$scope.ingredients = [""];
+
+	$scope.addIngredient = function() {
+		$scope.ingredients.push("");
+	};
 	
 	$scope.addRecipe = function() {
 		// If any of the fields are empty, don't add the recipe
-		if (!$scope.name || !$scope.ingredient1 || !$scope.instructions)
+		if (!$scope.name || !$scope.instructions)
 			return;
 
 		// Create ID for recipe
@@ -14,8 +19,13 @@ app.controller("mealPlannerCtrl", function($scope) {
 		var recipe = {};
 		recipe['name'] = $scope.name;
 		recipe['ingredients'] = [];
-		recipe['ingredients'].push($scope.ingredient1);
+		console.log($scope.ingredients);
+		for (var i = 0; i < $scope.ingredients.length; i++)
+		{
+			console.log($scope.ingredients[i]);
+			recipe['ingredients'].push($scope.ingredients[i]);
+		}
 		recipe['instructions'] = $scope.instructions;
 		$scope.recipelist[id] = recipe;
-	}
+	};
 });
