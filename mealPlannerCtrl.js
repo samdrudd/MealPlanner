90fdfefe644d998ae5de1fde02d1f85f0c3518a8
Sam Rudd <samdrudd@gmail.com>
2017-10-12 01:22:22 -0700

    now using node.js backend to store recipe using mongodb

--- mealPlannerCtrl.js
+++ mealPlannerCtrl.js
@@ -1,43 +1,39 @@
-app.controller("mealPlannerCtrl", function($scope) {
-	$scope.recipelist = {};
-	$scope.ingredients = [""];
+app.controller("mealPlannerCtrl", function($scope, $http) {
+	$scope.recipe = {};
+	$scope.recipe.ingredients = [""];
+	$scope.recipe.name = "";
+	$scope.recipe.instructions = "";
 
 	$scope.addIngredient = function() {
-		if ($scope.ingredients.length > 0 && $scope.ingredients[$scope.ingredients.length - 1].trim() != "")
-			$scope.ingredients.push("");
+		if ($scope.recipe.ingredients.length > 0 && $scope.recipe.ingredients[$scope.recipe.ingredients.length - 1].trim() != "")
+			$scope.recipe.ingredients.push("");
 	};
 	
 	$scope.addRecipe = function() {
 		// If any of the fields are empty, don't add the recipe
-		if (!$scope.name || !$scope.instructions)
+		if (!$scope.recipe.name || !$scope.recipe.instructions || $scope.recipe.ingredients == [""])
 			return;
 
-		// Create ID for recipe
-		var today = new Date();
-		var id = today.getTime();
+		$http({
+			url: "http://127.0.0.1:8000/recipe",
+			method: "POST",
+			data: $.param($scope.recipe),
+			headers: { "Content-Type": "application/x-www-form-urlencoded"}})
+		.then(function successCallback(response) {
+			console.log(response.data);
+		}, function errorCallback(response) {
+			console.log(response.statusText);
+		});
 
-		// Create recipe object
-		var recipe = {};
-		recipe['name'] = $scope.name;
-		recipe['ingredients'] = [];
-		
-		for (var i = 0; i < $scope.ingredients.length; i++)
-		{
-			console.log($scope.ingredients[i]);
-			recipe['ingredients'].push($scope.ingredients[i]);
-		}
-		
-		recipe['instructions'] = $scope.instructions;
-		$scope.recipelist[id] = recipe;
 	};
 
 	$scope.removeIngredient = function(id) {
-		$scope.ingredients.splice(id,1);
+		$scope.recipe.ingredients.splice(id,1);
 	}
 
 	$scope.resetForm = function() {
-		$scope.name = "";
-		$scope.ingredients = [""];
-		$scope.instructions = "";
+		$scope.recipe.name = "";
+		$scope.recipe.ingredients = [""];
+		$scope.recipe.instructions = "";
 	}
 });
